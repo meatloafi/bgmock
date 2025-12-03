3e35ab17c13bcca2f27edb3db4d254bb8b828d42
Felix Maathz <felixmaathz@hiq.se>
2025-12-03 13:20:24 +0100

    Changed path in scenarios

--- simple_simulator/scenarios/__init__.py
+++ simple_simulator/scenarios/__init__.py
@@ -1,6 +1,6 @@
 # scenarios/__init__.py
 from scenarios.kafka_outage_scenario import KafkaOutageScenario
-from simple_simulator.scenarios.interbank_transaction import InterbankTransactionScenario
+from scenarios.interbank_transaction import InterbankTransactionScenario
 
 SCENARIOS = [
     InterbankTransactionScenario,
